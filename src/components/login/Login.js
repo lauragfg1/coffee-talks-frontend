7fe7b56ff58ed1bca4c7e8dad4f8d84cb0229f83
lauragfg1 <lauragranda@uma.es>
2024-08-30 14:20:39 +0200

    Login improvements

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,7 @@
 import './Login.css';
 import React from "react";
 import Button from "../button/Button";
+import StaticBackground from "../staticBackground/StaticBackground";
 
 function Login() {
     const handleClick = () => {
@@ -9,6 +10,7 @@ function Login() {
 
     return (
         <div className="Login">
+            <StaticBackground/>
             <h1>Welcome!</h1>
             <p>In order to continue, you will need to log in using your Endava Account</p>
             <Button onClick={handleClick}>Log in</Button>
